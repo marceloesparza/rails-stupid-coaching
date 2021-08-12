98d9a02908b562f63cccf0cb5f9be91968a9ada6
Marcelo Esparza <marceloesparza@gmail.com>
2021-08-12 13:02:41 -0400

    only html, not css

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,6 +3,15 @@ class QuestionsController < ApplicationController
   end
 
   def answer
-    @question = params[:questions]
+    @question = params[:question]
+    if @question.empty? 
+      @answer = "you do not say anything!!"
+    elsif @question == "I am going to work"
+      @answer = "Great!"
+    elsif @question.include?("?")
+      @answer = "Silly question, get dressed and go to work!"
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
   end
 end
